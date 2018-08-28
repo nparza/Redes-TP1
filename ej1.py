dc04c038b5e8d3a5cd396ca5613cdcd0d1aac921
sofinico <nicolettisofia1@gmail.com>
2018-08-28 08:43:20 -0300

    Hasta ej 1. b) iii

--- ej1.py
+++ ej1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import networkx as nx
 from matplotlib import pyplot as plt
+import pandas as pd
 from func import *
 
 #%% Importo los datos y creo los grafos
@@ -14,18 +15,14 @@ LIT=ldata('/Users/sofinico/Documents/Exactas/Redes complejas'+
 APMS=ldata('/Users/sofinico/Documents/Exactas/Redes complejas'+
            '/Redes-TP1/tc01_data/yeast_AP-MS.txt')
 
-G_Y2H=nx.Graph(); G_LIT=nx.Graph(); G_APMS=nx.Graph();
-
-G_Y2H.add_edges_from(Y2H) 
-G_LIT.add_edges_from(LIT)
-G_APMS.add_edges_from(APMS)
+G_Y2H=creategraph(Y2H); G_LIT=creategraph(LIT); G_APMS=creategraph(APMS); 
 
 #%% Grafico
 
 f, (ax1,ax2,ax3) = plt.subplots(1,3)
 
 plt.sca(ax1)
-nx.draw(G_Y2H, node_size=10)
+nx.draw(G_Y2H, node_size=10,arrowstyle='-|>',width=0.5,arrowsize=3)
 ax1.set_title('Y2H')
 
 plt.sca(ax2)
@@ -40,5 +37,42 @@ f=plt.figure(1)
 plt.show()
 
 #%%
+
+nodes=[G_Y2H.number_of_nodes(),G_LIT.number_of_nodes(),G_APMS.number_of_nodes()]
+edges=[G_Y2H.number_of_edges(),G_LIT.number_of_edges(),G_APMS.number_of_edges()]
+
+caract = pd.DataFrame({ "Red":["Y2H","LIT","APMS"], 
+                        "N":nodes,
+                        "L":edges
+                      })
+print(caract)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- func.py
+++ func.py
@@ -1,3 +1,5 @@
+import networkx as nx
+
 def ldata(archive):
     f=open(archive)
     data=[]
@@ -7,6 +9,30 @@ def ldata(archive):
         data.append(col)	
     return data
 
-#def directed(lista):
-#    while  isdirected=False:
+def directed(lista):
+    i=0
+    isdirected=False
+    while i<len(lista) and isdirected==False:
+        par=lista[i]
+        j=i+1
+        while j<len(lista) and isdirected==False:
+            if par[0] in lista[j] and par[1] in lista[j]:
+                isdirected=True
+            j+=1
+        i+=1
+    return isdirected
+
+def creategraph(lista):
+    if directed(lista)==True:
+        graph=nx.DiGraph()
+    else: 
+        graph=nx.Graph()
+    graph.add_edges_from(lista)
+    return graph
+    
+        
+            
+        
+        
+        
         
