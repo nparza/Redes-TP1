94f95ba692427e93fb24d7660d18814b0484ffab
nparza <noelparzajuk@hotmail.com>
2018-09-17 01:15:23 -0300

    Script y jupyter del ej 2

--- ej2.py
+++ ej2.py
@@ -9,6 +9,7 @@ import numpy as np
 import networkx as nx
 from matplotlib import pyplot as plt
 from itertools import chain
+from func2 import *
 
 
 #%%
@@ -32,39 +33,43 @@ nx.draw(dolphins,
         with_labels=True,
         node_color= ['blue' if g=="m" else "r" for g in nx.get_node_attributes(dolphins, "gender").values()] , 
         node_size=10)
-#%%
-nx.draw_shell(dolphins, 
-        with_labels=True,
-        node_color= ['blue' if g=="m" else "r" for g in nx.get_node_attributes(dolphins, "gender").values()] , 
-        node_size=10, nlist=[range(5,10), range(5)])
+
+
 #%%
 
 
 
-times = 5000
+times = 10000
 fraccion = []
-fraccion_medida = straight(dolphins)[1]/dolphins.number_of_nodes()
+fraccion_medida = straight(dolphins)/dolphins.number_of_edges()
 
 
 for n in range(times):
-    dolphins = bend(dolphins)
-    fraccion.append(straight(dolphins)[1]/dolphins.number_of_nodes())
+    dolphins = bend(dolphins, gender, name_attribute='gender')
+    fraccion.append(straight(dolphins)/dolphins.number_of_edges())
     
+
 
 freq, binedges = np.histogram(fraccion, bins=20)
 norm = sum(freq)
 freq_normed = [i/norm for i in freq]
 bincenters = 0.5*(binedges[1:]+binedges[:-1])
-pval = pval(freq_normed, binedges, fraccion_medida)
 lins = {'linestyle': 'None'}
 
+mean = np.mean(fraccion)
+stdev = np.std(fraccion)
+### El p-val corresponde a la probabilidad de obtener un resultado mayor o igual al obtenido
+p_val = pval(freq_normed, binedges, fraccion_medida)
+
 plt.rc('lines', **lins)
+plt.axvline(x=fraccion_medida, color='k', linestyle='dashed', linewidth=1)
 plt.bar(bincenters,freq_normed, color='orange',edgecolor='black', linewidth=1.2, width = np.diff(binedges))
 plt.grid(True)
 plt.ylabel('Frecuencia')
-plt.xlabel('Fracción de parejas heteronormativas. Valor medido %s. p-val %s' %(round(fraccion_medida,2),round(pval,2) ))
+plt.xlabel('Fracción de parejas heteronormativas.' )
+plt.title('Fracción medida %s. p-val %s. Media de la distribución: %s ±% s ' 
+%(round(fraccion_medida,2),round(p_val,2),round(mean,2 ),round(stdev,2) ))
 plt.show()
-
 
 
 
