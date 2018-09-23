f37c5cbeb5d596b6f7b3923c709628845ec40885
sofinico <nicolettisofia1@gmail.com>
2018-09-23 03:22:35 -0300

    ej1 terminado by sofi. falta chequeo noe

--- ej1.py
+++ ej1.py
@@ -20,18 +20,25 @@ G_Y2H.add_edges_from(Y2H); G_LIT.add_edges_from(LIT); G_APMS.add_edges_from(APMS
 
 #%% Grafico
 
+def draw_GraphStyle(G):
+    nx.draw(G,
+            node_size=3, 
+            node_color='dodgerblue',
+            edge_color='grey'
+            )
+
 f, (ax1,ax2,ax3) = plt.subplots(1,3)
 
 plt.sca(ax1)
-nx.draw(G_Y2H, node_size=10)
+draw_GraphStyle(G_Y2H)
 ax1.set_title('Y2H')
 
 plt.sca(ax2)
-nx.draw(G_LIT, node_size=10)
+draw_GraphStyle(G_LIT)
 ax2.set_title('LIT')
 
 plt.sca(ax3)
-nx.draw(G_APMS, node_size=10)
+draw_GraphStyle(G_APMS)
 ax3.set_title('APMS')
 
 f=plt.figure(1)
@@ -42,7 +49,7 @@ plt.show()
 redes=[G_Y2H,G_LIT,G_APMS]
 
 nodes=[]; edges=[]; kmedio=[]; kmax=[]; kmin=[]; 
-clust1=[]; clust2=[]; densidad=[]; diam=[];
+clustl=[]; clustg=[]; densidad=[]; diam=[];
 
 pos=0
 for red in redes:
@@ -55,8 +62,8 @@ for red in redes:
     densidad.append(edges[pos]/(nodes[pos]*(nodes[pos]-1)/2))
     compgigante=max(nx.connected_component_subgraphs(red),key=len)
     diam.append(nx.diameter(compgigante))
-    clust1.append(nx.average_clustering(red))
-    clust2.append(nx.transitivity(red))
+    clustl.append(nx.average_clustering(red))
+    clustg.append(nx.transitivity(red))
     pos+=1
     
 esdirigida=[False]*3 #Interacción entre proteínas: no dirigido
@@ -65,73 +72,97 @@ esdirigida=[False]*3 #Interacción entre proteínas: no dirigido
     
 #%% Grafico tabla
 
-caract = pd.DataFrame({ "Red":["Y2H","LIT","APMS"], 
-                        "N":nodes,
-                        "L":edges,
-                        "Dirigida":esdirigida,
-                        "<k>":kmedio,
-                        "k máx":kmax,
-                        "k mín":kmin,
-                        "Densidad":densidad,
-                        "Clust. <Ci>":clust1,
-                        "Clust. global":clust2,
-                        "Diametro":[np.inf]*3,
-                        "Diam. comp. gigante":diam
+caract = pd.DataFrame({ 'red':['Y2H','LIT','APMS'], 
+                        'N':nodes,
+                        'L':edges,
+                        'k_medio':kmedio,
+                        'k_max':kmax,
+                        'k_min':kmin,
+                        'densidad':densidad,
+                        'c_local':clustl,
+                        'c_global':clustg,
+                        'diam_CG':diam
                       })
+
+caract = caract[['red','N','L','k_medio',
+                 'k_max','k_min','densidad','c_local',
+                 'c_global','diam_CG']]
+
 #Uso cols para copiar y pegar y hacer más rapido la redefinición de lugares
 cols=list(caract.columns.values)
 
-caract=caract[['Red','N','L','Dirigida','<k>',
-               'k máx','k mín','Densidad','Clust. <Ci>',
-               'Clust. global','Diametro','Diam. comp. gigante']]
 print(caract)
 
 #%% Agrego algunos atributos relacionados a los componentes
 
 for red in redes:
-    red.graph["Componentes"]=list(nx.connected_component_subgraphs(red))
-    compgigante=max(red.graph["Componentes"],key=len)
-    red.graph["Componentes"].remove(compgigante)
-    clust_comp=[]
-    trans_comp=[]
-    for comp in red.graph["Componentes"]:
-        clust_comp.append(nx.average_clustering(comp))
-        trans_comp.append(nx.transitivity(comp))
-    red.graph["<Ci> de cada componente"]=clust_comp
-    red.graph["Cglobal de cada componente"]=trans_comp
-    red.graph["<Ci> CG"]=nx.average_clustering(compgigante)
-    red.graph["Cglobal CG"]=nx.transitivity(compgigante)
+    red.graph["comp"]=list(nx.connected_component_subgraphs(red))
+    compgigante=max(red.graph["comp"],key=len)
+    red.graph["comp"].remove(compgigante)
+    nCG=compgigante.number_of_nodes()
+    eCG=compgigante.number_of_edges()
+    red.graph["dens_CG"]=eCG/(nCG*(nCG-1)/2)
+    clustl_comp=[]; clustg_comp=[]; 
+    kmedio_comp=[]; dens_comp=[];
+    for comp in red.graph["comp"]:
+        clustl_comp.append(nx.average_clustering(comp))
+        clustg_comp.append(nx.transitivity(comp))
+        kcomp=degrees2list(comp)
+        kmedio_comp.append(np.mean(kcomp))
+        n=comp.number_of_nodes()
+        e=comp.number_of_edges()
+        if n!=1:
+            dens_comp.append(e/(n*(n-1)/2))
+        else:
+            dens_comp.append(0)
+    red.graph["c_local_comp"]=clustl_comp
+    red.graph["c_global_comp"]=clustg_comp
+    red.graph["k_medio_comp"]=kmedio_comp
+    red.graph["densidad_comp"]=dens_comp
+    red.graph["c_local_CG"]=nx.average_clustering(compgigante)
+    red.graph["c_global_CG"]=nx.transitivity(compgigante)
+    
 
 #%% Tablita comparativa sobre los componentes de cada grafo
     
-n_comps=[]; n_clust1=[]; n_trans1=[]; promc=[]; promt=[];
-clust_cg=[]; trans_cg=[];
+n_comps=[]; n_clustl1=[]; n_clustg1=[]; prom_cl=[]; prom_cg=[];
+clustl_CG=[]; clustg_CG=[]; prom_kmedio_comps=[]; prom_dens_comp=[];
+dens_cg=[];
 
 for red in redes:
-    n_comps.append(len(red.graph["Componentes"]))
-    clust_comp=red.graph["<Ci> de cada componente"]
-    trans_comp=red.graph["Cglobal de cada componente"]
-    promc.append(np.mean(clust_comp))
-    promt.append(np.mean(trans_comp))
-    n_clust1.append(sum([int(i==1) for i in clust_comp]))
-    n_trans1.append(sum([int(i==1) for i in trans_comp]))
-    clust_cg.append(red.graph["<Ci> CG"])
-    trans_cg.append(red.graph["Cglobal CG"])
+    n_comps.append(len(red.graph["comp"]))
+    clustl_comp=red.graph["c_local_comp"]
+    clustg_comp=red.graph["c_global_comp"]
+    prom_cl.append(np.mean(clustl_comp))
+    prom_cg.append(np.mean(clustg_comp))
+    n_clustl1.append(sum([int(i==1) for i in clustl_comp]))
+    n_clustg1.append(sum([int(i==1) for i in clustg_comp]))
+    clustl_CG.append(red.graph["c_local_CG"])
+    clustg_CG.append(red.graph["c_global_CG"])
+    prom_kmedio_comps.append(np.mean(red.graph["k_medio_comp"]))
+    prom_dens_comp.append(np.mean(red.graph["densidad_comp"]))
+    dens_cg.append(red.graph["dens_CG"])
     
-
-caract2 = pd.DataFrame({ "Red":["Y2H","LIT","APMS"],
-                         "Nº componentes":n_comps,
-                         "Prom. <Ci> de c/ comp.":promc,
-                         "Prom. clust. global de c/ comp":promt,
-                         "<Ci> CG":clust_cg,
-                         "Clust. global CG":trans_cg,
-                         "Comps con <Ci>=1":n_clust1,
-                         "Comps con clust. global=1":n_trans1
+    
+caract2 = pd.DataFrame({ "red":["Y2H","LIT","APMS"],
+                         "n_comps":n_comps,
+                         "prom_c_local_comp":prom_cl,
+                         "prom_c_global_comp":prom_cg,
+                         "c_local_CG":clustl_CG,
+                         "c_global_CG":clustg_CG,
+                         "n_c_local_1":n_clustl1,
+                         "n_c_global_1":n_clustg1,
+                         "prom_dens_comp":prom_dens_comp,
+                         "dens_CG":dens_cg,
+                         "prom_kmedio_comps":prom_kmedio_comps
                          })
+    
+caract2=caract2[['red','n_comps','prom_c_local_comp','prom_c_global_comp',
+                 'n_c_local_1','n_c_global_1','c_local_CG','c_global_CG',
+                 'prom_kmedio_comps','prom_dens_comp','dens_CG']]
+    
 print(caract2)
 
-
-#Agregar <k> y densidad de cada componente
 
 
 
