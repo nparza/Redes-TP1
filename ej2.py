a1c8b0b2cab0efa141aeef0d8f8d4dfe89b4744e
sofinico <nicolettisofia1@gmail.com>
2018-09-25 03:59:32 -0300

    modularidad joya. funcion recableado a medias

--- ej2.py
+++ ej2.py
@@ -16,10 +16,8 @@ from func2 import *
 
 ## Load data
 
-dolphins = nx.read_gml('c:/users/noelp/Documents/Git'+
-           '/Redes-TP1/tc01_data/dolphins.gml')
-gender = ldata('c:/users/noelp/Documents/Git'+
-           '/Redes-TP1/tc01_data/dolphinsGender.txt')
+dolphins = nx.read_gml('TC01_data/dolphins.gml')
+gender = ldata('TC01_data/dolphinsGender.txt')
 gender = [gender[n][1] for n in range(len(gender))]
 
 for n,g in zip(dolphins.nodes,gender):
@@ -29,9 +27,11 @@ for n,g in zip(dolphins.nodes,gender):
 
 ### Layout
 
-nx.draw(dolphins, 
+nx.draw(dolphins2, 
         with_labels=True,
-        node_color= ['blue' if g=="m" else "r" for g in nx.get_node_attributes(dolphins, "gender").values()] , 
+        node_color= ['blue' if g=="m" 
+                     else "r" 
+                     for g in nx.get_node_attributes(dolphins, "gender").values()], 
         node_size=10)
 
 
@@ -41,18 +41,14 @@ nx.draw(dolphins,
 
 times = 1000
 fraccion = []
-#modul = []
 fraccion_medida = straight(dolphins)/dolphins.number_of_edges() ##Fracción de enlaces de distinto sexo
-#modul_medida = modulacion(dolphins, attribute='gender')
 
 
 for n in range(times):
     dolphins = bend(dolphins, gender, name_attribute='gender')
     fraccion.append(straight(dolphins)/dolphins.number_of_edges())
-#    modul.append(modulacion(dolphins,attribute='gender'))
+
     
-
-
 freq, binedges = np.histogram(fraccion, bins=20)
 norm = sum(freq)
 freq_normed = [i/norm for i in freq]
@@ -70,34 +66,93 @@ plt.axvline(x=fraccion_medida, color='k', linestyle='dashed', linewidth=1)
 plt.bar(bincenters,freq_normed, color='orange',edgecolor='black', linewidth=1.2, width = np.diff(binedges))
 plt.grid(True)
 plt.ylabel('Frecuencia')
-plt.xlabel('Fracción de parejas heteronormativas.' )
+plt.xlabel('Fracción de parejas heteronormativas' )
 plt.title('Fracción medida %s. p-val %s. Media de la distribución: %s ±% s ' 
 %(round(fraccion_medida,2),round(p_val,2),round(mean,2 ),round(stdev,2) ))
 plt.show(1)
 
 
-#freq, binedges = np.histogram(modul, bins=20)
-#norm = sum(freq)
-#freq_normed = [i/norm for i in freq]
-#bincenters = 0.5*(binedges[1:]+binedges[:-1])
-#lins = {'linestyle': 'None'}
-#
-#mean = np.mean(modul)
-#stdev = np.std(modul)
-#### El p-val corresponde a la probabilidad de obtener un resultado mayor o igual al obtenido
-#p_val = pval(freq_normed, binedges, fraccion_medida)
-#
-#plt.figure(2)
-#plt.rc('lines', **lins)
-#plt.axvline(x=modul_medida, color='k', linestyle='dashed', linewidth=1)
-#plt.bar(bincenters,freq_normed, color='green',edgecolor='black', linewidth=1.2, width = np.diff(binedges))
-#plt.grid(True)
-#plt.ylabel('Frecuencia')
-#plt.xlabel('Distribución de la modulación' )
-#plt.title('Modulación medida %s. p-val %s. Media de la distribución: %s ±% s ' 
-#%(round(modul_medida,2),round(p_val,2),round(mean,2 ),round(stdev,2) ))
-#plt.show(2)
-#
+#%% MODULARIDAD
+
+## Usando la función de networkx
+
+from networkx.algorithms.community import modularity
+from networkx.algorithms import community
+
+females = set(grupo(dolphins,attribute='gender',kind ='f'))
+males = set(grupo(dolphins,attribute='gender',kind ='m'))
+nonbinary = set(grupo(dolphins,attribute='gender',kind ='NA'))
+
+communities = [females, males, nonbinary]
+
+Q1 = modularity(dolphins,communities)
+
+
+## Usando la función manual creada por Noe
+
+Q2 = modularidad(dolphins,'gender')
+
+
+#%% 
+
+## Fracción de enlaces entre grupos - Comparación con modelo nulo
+## Modelo nulo: recableado de la red preservando la distribución de grado
+
+E = dolphins.number_of_edges()
+times = 1000
+fraccion = []
+
+def n_enlaces(graph, attribute, kind):
+    
+    return len(subgraph(graph, attribute, kind))
+
+fraccion_female = n_enlaces(dolphins,'gender','f')/E
+fraccion_male = n_enlaces(dolphins,'gender','m')/E
+fraccion_total = ((n_enlaces(dolphins,'gender','f') + 
+                  n_enlaces(dolphins,'gender','m'))/E)
+
+fraccion_medida = fraccion_total
+
+for n in range(times):
+    dolphins2 = recableado(dolphins)
+    fraccion.append((n_enlaces(dolphins2,'gender','f') +
+                     n_enlaces(dolphins2,'gender','m'))/E)
+
+freq, binedges = np.histogram(fraccion, bins=20)
+norm = sum(freq)
+freq_normed = [i/norm for i in freq]
+bincenters = 0.5*(binedges[1:]+binedges[:-1])
+lins = {'linestyle': 'None'}
+
+mean = np.mean(fraccion)
+stdev = np.std(fraccion)
+### El p-val corresponde a la probabilidad de obtener un resultado mayor o igual al obtenido
+p_val = pval(freq_normed, binedges, fraccion_medida)
+
+plt.figure(2)
+plt.rc('lines', **lins)
+plt.axvline(x=fraccion_medida, color='k', linestyle='dashed', linewidth=1)
+plt.bar(bincenters,freq_normed, color='green',edgecolor='black', linewidth=1.2, width = np.diff(binedges))
+plt.grid(True)
+plt.ylabel('Frecuencia')
+plt.xlabel('Fracción de enlaces dentro del mismo género' )
+plt.title('Fracción medida %s. p-val %s. Media de la distribución: %s ±% s ' 
+%(round(fraccion_medida,2),round(p_val,2),round(mean,2 ),round(stdev,2) ),fontsize=10)
+plt.show(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- func2.py
+++ func2.py
@@ -56,12 +56,14 @@ def subgraph(graph,attribute='gender', kind='f'):
     edges = list(dict(graph.edges))
     subgraph = []
     for n in range(graph.number_of_edges()):
-        if graph.nodes[edges[n][0]] == graph.nodes[edges[n][1]] and graph.nodes[edges[n][0]] == kind :
+        if graph.nodes[edges[n][0]] == graph.nodes[edges[n][1]] and graph.nodes[edges[n][0]]['gender'] == kind :
             subgraph.append(edges[n])
     return subgraph
     
-def modulacion(graph, attribute='gender'):
-    #### Calcula la modulación. REVISAR ESTO!!!!!!
+def modularidad(graph, attribute='gender'):
+    
+    #### Calcula la modularidad de la red
+    
     q = 0
     m = (2*graph.number_of_edges())
     for i in range(graph.number_of_nodes()):
@@ -75,14 +77,14 @@ def modulacion(graph, attribute='gender'):
                 kj = graph.degree()[nj]
                 aij = nx.adjacency_matrix(graph)[i,j]
 
-                qij = aij - ki*kj/(2*graph.number_of_edges())
+                qij = aij - ki*kj/m
             else:
                 qij = 0
             q += qij
             
     return q/m
+ 
     
-
 ##Reasigna un atributo aleatoriamente manteniendo la proporción
 ## original de dicho atributo
 def bend(graph, attribute, name_attribute='gender'):
@@ -108,4 +110,73 @@ def pval(freq,bines,Tobs):
             else:
                 break
         return 1 - cumprob[i-1] 
+    
+
+## ANDA, PERO NO CALCULA LO QUE QUIERO
+# Cunado reasigno los edges, no contemplo que se me pueden armar pares repetidos
+# o loops
+
+def recableado(graph):
+    
+    ## Reasigna los enlaces manteniendo la distribución de grado de cada nodo
+    
+    half_edges = []
+    for n in list(graph.nodes()):
+        half_edges.extend([n]*graph.degree()[n])
+    
+    np.random.shuffle(half_edges)
+    
+    new_edges = []
+    for i in range(0,len(half_edges)-1,2):
+        new_edges.append([half_edges[i],half_edges[i+1]])
+
+    # Arreglo posibles loops
+#    while i<len(new_edges):
+#        if new_edges[i][0] == new_edges[i][1]:
+#            # Separo en dos casos por si el último edge es un loop
+#            if i == len(new_edges)-1:
+#                np.random.shuffle(new_edges)
+#                i = -1
+#            else:
+#                nodo = new_edges[i][0] 
+#                new_edges[i][0] = new_edges[i+1][0]
+#                new_edges[i+1][0] = nodo
+#                i -= 1  
+#        i += 1
+#    
+    # Creo el grafo recableado
+    new_graph = nx.Graph()
+    new_graph.add_edges_from(new_edges)
+    
+    #Recupero los géneros del grafo original
+    for n in list(graph.nodes()):
+        new_graph.nodes[n]['gender'] = graph.nodes[n]['gender']
+    
+    return new_graph
+    
+    
+            
+            
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
